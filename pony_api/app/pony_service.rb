41d882c80e4a8035088ce50eb7125695fe0e29df
Dirk Breuer <dirk.breuer@gmail.com>
2012-04-18 18:01:12 +0200

    calling the remote pony service via BERT-RPC

--- pony_api/app/pony_service.rb
+++ pony_api/app/pony_service.rb
@@ -1,13 +1,19 @@
+require 'bertrpc'
+
 class PonyService
   class NotFound < Exception; end
 
   class <<self
     def find_pony_by_name(name)
-      if name == "Applejack"
-        { name: "Applejack", type: "Earthpony" }
+      if pony = client.call.pony_service.find_pony_by_name(name)
+        return pony
       else
         raise NotFound
       end
+    end
+
+    def client
+      @client ||= BERTRPC::Service.new('localhost', 8000)
     end
   end
 
